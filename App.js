fc3a5e814cfbbf5601216b1ead5a7112f807fa59
Sashankr <sashankr97@gmail.com>
2022-09-13 18:21:30 +0530

    Practiced usestate in react native

--- App.js
+++ App.js
@@ -6,19 +6,22 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {StatusBar, Text, View, StyleSheet, Button, Linking} from 'react-native';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [timesClicked, setTimesClicked] = useState(0);
+
+  const clickHandler = () => {
+    setCount(count => count + 5);
+    setTimesClicked(timesClicked => timesClicked + 1);
+  };
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Hello World</Text>
-      <Button
-        title="Go"
-        onPress={() => {
-          Linking.openURL('https://sashankr.com');
-        }}
-      />
+      <Text style={styles.text}>{count}</Text>
+      <Button title="Add 5" onPress={clickHandler} />
+      <Text style={styles.text}>You clicked {timesClicked} times.</Text>
     </View>
   );
 };
